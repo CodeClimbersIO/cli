a052da5ff7e978c1eb19566f58bed71f14588529
Danielle Hovley <djl.hovley@gmail.com>
2024-07-17 11:47:55 -0600

    added logo;

--- src/app/src/components/Home/HomePage.tsx
+++ src/app/src/components/Home/HomePage.tsx
@@ -1,8 +1,10 @@
 import { LatestPulsesComponent } from './LatestPulses'
+import Logo from './Logo'
 
 export const HomePage = () => {
   return (
     <div>
+      <Logo />
       <h1>Home</h1>
       <LatestPulsesComponent />
     </div>
---
+++ src/app/src/components/Home/Logo.tsx
@@ -0,0 +1,11 @@
+import { useTheme } from '@mui/material'
+import blackLogo from '../../public/images/logo.png'
+import whiteLogo from '../../public/images/logo-white.png'
+
+const Logo = () => {
+  const logo = useTheme().palette.mode === 'dark' ? whiteLogo : blackLogo
+
+  return <img src={logo} />
+}
+
+export default Logo
