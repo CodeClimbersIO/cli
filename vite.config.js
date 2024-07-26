54e6c8d210024aee74a53aac99afea13da5cc0ce
Paul Hovley <rphovley@gmail.com>
2024-07-25 19:53:32 -0600

    Update to be able to publish to npm from our project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src/app',
+  root: 'packages/app',
   build: {
     outDir: '../../dist/app',
     emptyOutDir: true,
