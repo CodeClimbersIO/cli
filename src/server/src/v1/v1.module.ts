1ad1911a066edc7bd3b492d2d99d0421a91416b1
Jorge Duran <jorgeluis21.duran@gmail.com>
2024-07-27 15:27:10 -0400

    style: :art: Health controller should have not been removed
    Health controller should have not been removed
    
    Health controller should have not been removed

--- src/server/src/v1/v1.module.ts
+++ src/server/src/v1/v1.module.ts
@@ -4,10 +4,11 @@ import { PulseRepo } from './pulse/infrastructure/database/pulse.repo'
 import { PulseController } from './pulse/infrastructure/http/controllers/pulse.controller'
 import { WakatimeController } from './pulse/infrastructure/http/controllers/wakatimeProxy.controller'
 import { StartupService } from './startup/application/services/startup.service'
+import { HealthController } from '../common/infrastructure/http/controllers/health.controller'
 
 @Module({
   imports: [],
-  controllers: [PulseController, WakatimeController],
+  controllers: [HealthController, PulseController, WakatimeController],
   providers: [ActivitiesService, StartupService, PulseRepo],
 })
 export class V1Module {}
