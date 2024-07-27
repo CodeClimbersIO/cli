db353be84c9bf96bc3506ad3386abaf91ca7ceca
Dani <djl.hovley@gmail.com>
2024-07-27 16:31:16 -0600

    Get Week Overview Endpoint (#28)
    Co-authored-by: Paul Hovley <rphovley@gmail.com>

--- src/server/src/v1/pulse/infrastructure/database/pulse.repo.ts
+++ src/server/src/v1/pulse/infrastructure/database/pulse.repo.ts
@@ -2,12 +2,15 @@ import { Injectable, Logger } from '@nestjs/common'
 import { InjectKnex, Knex } from 'nestjs-knex'
 import sqlReaderUtil from '../../../../../utils/sqlReader.util'
 
+interface MinutesQuery {
+  minutes: number
+}
+
 @Injectable()
 export class PulseRepo {
   constructor(@InjectKnex() private readonly knex: Knex) {}
 
   tableName = 'activities_pulse'
-  pulseDb = this.knex<CodeClimbers.Pulse>(this.tableName)
 
   async getStatusBarDetails(): Promise<CodeClimbers.WakatimePulseStatusDao[]> {
     const getTimeQuery = await sqlReaderUtil.getFileContentAsString(
@@ -22,14 +25,58 @@ export class PulseRepo {
     return res
   }
 
+  getMinutesInRangeQuery(startDate: Date, endDate: Date) {
+    return this.knex<MinutesQuery[]>(this.tableName)
+      .count('* as minutes')
+      .from(this.tableName)
+      .whereBetween('time', [startDate.toISOString(), endDate.toISOString()])
+      .groupBy(this.knex.raw("strftime('%s', time) / 60"))
+  }
+
+  async getLongestDayInRangeMinutes(
+    startDate: Date,
+    endDate: Date,
+  ): Promise<number> {
+    const getLongestDayMinutesQuery =
+      await sqlReaderUtil.getFileContentAsString(
+        'getLongestDayInRangeMinutes.sql',
+      )
+    const [result] = await this.knex.raw<MinutesQuery[]>(
+      getLongestDayMinutesQuery,
+      {
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+      },
+    )
+
+    return result.minutes
+  }
+
+  async getRangeMinutes(startDate: Date, endDate: Date): Promise<number> {
+    const result: MinutesQuery = await this.knex<MinutesQuery[]>(this.tableName)
+      .with('getMinutes', this.getMinutesInRangeQuery(startDate, endDate))
+      .count('* as minutes')
+      .first()
+      .from('getMinutes')
+
+    return result.minutes
+  }
+
   async getCategoryTimeOverview(
     startDate: string,
     endDate: string,
-  ): Promise<CodeClimbers.TimeOverviewDao[] | undefined> {
-    const getTimeQuery = await sqlReaderUtil.getFileContentAsString(
-      'getCategoryTimeOverview.sql',
-    )
-    return this.knex.raw(getTimeQuery, { startDate, endDate })
+  ): Promise<CodeClimbers.TimeOverviewDao[]> {
+    const query = this.knex<MinutesQuery[]>(this.tableName)
+      .select(this.knex.raw('category, count()'))
+      .from(this.tableName)
+      .whereBetween('time', [startDate, endDate])
+      .groupBy(this.knex.raw("strftime('%s', time) / 60"))
+
+    return await this.knex<CodeClimbers.TimeOverviewDao[]>(this.tableName)
+      .with('getMinutes', query)
+      .select(this.knex.raw('category, count() as minutes'))
+      .groupBy('category')
+      .from('getMinutes')
   }
 
   async createPulse(pulse: CodeClimbers.Pulse) {
--- src/server/src/v1/pulse/infrastructure/http/controllers/pulse.controller.ts
+++ src/server/src/v1/pulse/infrastructure/http/controllers/pulse.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { ActivitiesService } from '../../../application/services/activities.service'
 import { GetCategoryTimeOverviewDto } from '../../../application/dtos/getCategoryTimeOverview.dto'
+import { GetWeekOverviewDto } from '../../../application/dtos/getWeekOverview.dto'
 
 @Controller('pulses')
 export class PulseController {
@@ -14,6 +15,14 @@ export class PulseController {
   }> {
     const pulse = await this.activitiesService.getLatestPulses()
     return { message: 'success', data: pulse }
+  }
+
+  @Get('weekOverview')
+  async getWeekOverview(
+    @Query() dto: GetWeekOverviewDto,
+  ): Promise<CodeClimbers.WeekOverviewDao> {
+    const result = await this.activitiesService.getWeekOverview(dto.date)
+    return result
   }
 
   @Get('categoryTimeOverview')
