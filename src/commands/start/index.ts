841e8bdf88c7c5d886f560202888a7b167d81a69
Tanner Scadden <tanner@scaddenfamily.com>
2024-07-27 15:49:31 -0400

    Lint

--- src/commands/start/index.ts
+++ src/commands/start/index.ts
@@ -1,25 +1,25 @@
 // src/commands/start/index.ts
-process.env.APP_CONTEXT = "cli";
+process.env.APP_CONTEXT = 'cli'
 
-import { Args, Command } from "@oclif/core";
+import { Args, Command } from '@oclif/core'
 // eslint-disable-next-line import/no-unresolved
-import { bootstrap } from "server";
+import { bootstrap } from 'server'
 
 export default class Start extends Command {
   static args = {
     port: Args.string({
-      description: "Custom port to run the server on",
+      description: 'Custom port to run the server on',
       required: false,
     }),
-  };
+  }
 
-  static description = "Starts the codeclimbers server on your machine";
+  static description = 'Starts the codeclimbers server on your machine'
 
-  static examples = [`<%= config.bin %> <%= command.id %>`];
+  static examples = [`<%= config.bin %> <%= command.id %>`]
 
   async run(): Promise<void> {
-    const { args } = await this.parse(Start);
-    process.env.PORT = args.port || "14400"; // number of minutes in a day times 10
-    bootstrap();
+    const { args } = await this.parse(Start)
+    process.env.PORT = args.port || '14400' // number of minutes in a day times 10
+    bootstrap()
   }
 }
