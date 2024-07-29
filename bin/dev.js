2d3057df314e96732b6042ccdc74f0f9f3f167b1
Tanner Scadden <tanner@scaddenfamily.com>
2024-07-28 23:00:01 -0400

    Added watch mode to the dev cli to reload on changes under the src & bin folders

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env node_modules/.bin/ts-node
+#!/usr/bin/env node --watch --watch-path=./src --watch-path=./bin --loader=ts-node/esm
 // eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
 ;(async () => {
   const oclif = await import('@oclif/core')
-  await oclif.execute({development: true, dir: __dirname})
+  await oclif.execute({ development: true, dir: __dirname })
 })()
