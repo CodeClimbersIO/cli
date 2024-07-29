9e39649c7f052c330c50cbf2a3b779c50be5f0da
Tanner Scadden <44242867+Tanner-Scadden@users.noreply.github.com>
2024-07-28 21:17:59 -0600

    [Issue 46] CLI: stop command (#50)

---
+++ src/commands/stop/index.ts
@@ -0,0 +1,42 @@
+// src/commands/start/index.ts
+process.env.APP_CONTEXT = 'cli'
+
+import { Command } from '@oclif/core'
+import { exec as _exec } from 'node:child_process'
+import util from 'node:util'
+import os from 'node:os'
+import find from 'find-process'
+// eslint-disable-next-line import/no-unresolved
+import { SERVER_CONSTANTS } from 'server'
+
+const exec = util.promisify(_exec)
+
+const IS_WINDOWS = os.platform() === 'win32'
+
+export default class Stop extends Command {
+  static description = 'Stops the codeclimbers server on your machine'
+
+  static examples = [`<%= config.bin %> <%= command.id %>`]
+
+  async run(): Promise<void> {
+    const [instance] = await find('name', SERVER_CONSTANTS.PROCESS_NAME)
+
+    if (!instance) {
+      this.error(
+        `Could not find a running instance of ${SERVER_CONSTANTS.PROCESS_NAME}`,
+      )
+    }
+
+    const { stdout, stderr } = await exec(
+      IS_WINDOWS
+        ? `taskkill /F /PID ${instance.pid}`
+        : `kill -9 ${instance.pid}`,
+    )
+
+    if (stderr) {
+      this.error(stderr)
+    }
+
+    this.log(`Stopped ${SERVER_CONSTANTS.PROCESS_NAME} - ${stdout}`)
+  }
+}
