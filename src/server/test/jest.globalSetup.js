e023d01121968beba3fc369401e43864d1d3d1e4
Paul Hovley <rphovley@gmail.com>
2024-08-01 14:05:25 -0600

    Fix tests

--- src/server/test/jest.globalSetup.js
+++ src/server/test/jest.globalSetup.js
@@ -1,10 +1,9 @@
 const Knex = require('knex')
 const {
-  knexConfig,
+  knex,
   SQL_LITE_TEST_FILE,
 } = require('../src/v1/pulse/infrastructure/database/knex')
 
-const knex = Knex(knexConfig)
 module.exports = async () => {
   if (knex.client.config.connection.filename !== SQL_LITE_TEST_FILE) {
     throw new Error('You must use an in-memory database')
