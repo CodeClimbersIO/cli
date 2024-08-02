203c89bc1c8911533ca21fa602c20ae8fd7d6eb1
Tanner Scadden <tanner@scaddenfamily.com>
2024-08-01 22:21:50 -0400

    Lint

--- src/app/src/components/Home/Source/Sources.empty.tsx
+++ src/app/src/components/Home/Source/Sources.empty.tsx
@@ -5,19 +5,19 @@ import {
   Typography,
   Button,
   useTheme,
-} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import AddSources from "./AddSources";
-import { useState } from "react";
+} from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import AddSources from './AddSources'
+import { useState } from 'react'
 
 const SourcesEmpty = () => {
-  const [addSourcesOpen, setAddSourcesOpen] = useState(false);
-  const theme = useTheme();
+  const [addSourcesOpen, setAddSourcesOpen] = useState(false)
+  const theme = useTheme()
 
   return (
     <>
-      <Card sx={{ boxShadow: "none", borderRadius: 0, minWidth: 262 }}>
-        <CardContent sx={{ padding: "24px", height: "100%", display: "flex" }}>
+      <Card sx={{ boxShadow: 'none', borderRadius: 0, minWidth: 262 }}>
+        <CardContent sx={{ padding: '24px', height: '100%', display: 'flex' }}>
           <Stack direction="column">
             <Stack direction="row" justifyContent="space-between">
               <Typography variant="h3" alignContent="center" textAlign="left">
@@ -29,13 +29,13 @@ const SourcesEmpty = () => {
                 onClick={() => setAddSourcesOpen(true)}
                 sx={{
                   backgroundColor:
-                    theme.palette.mode === "dark" ? "#EBEBEB" : "#1F2122",
-                  borderRadius: "2px",
-                  textTransform: "none",
-                  display: "flex",
-                  alignItems: "center",
-                  width: "auto",
-                  height: "32px",
+                    theme.palette.mode === 'dark' ? '#EBEBEB' : '#1F2122',
+                  borderRadius: '2px',
+                  textTransform: 'none',
+                  display: 'flex',
+                  alignItems: 'center',
+                  width: 'auto',
+                  height: '32px',
                   minWidth: 0,
                 }}
               >
@@ -58,7 +58,7 @@ const SourcesEmpty = () => {
         handleClose={() => setAddSourcesOpen(false)}
       />
     </>
-  );
-};
+  )
+}
 
-export default SourcesEmpty;
+export default SourcesEmpty
