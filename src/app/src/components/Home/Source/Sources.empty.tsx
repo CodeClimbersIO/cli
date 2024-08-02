982d426309d3d98d8cad7b85d6b8efa5eff166c2
Tanner Scadden <tanner@scaddenfamily.com>
2024-08-01 22:10:13 -0400

    Added  and state for it in empty

--- src/app/src/components/Home/Source/Sources.empty.tsx
+++ src/app/src/components/Home/Source/Sources.empty.tsx
@@ -5,50 +5,60 @@ import {
   Typography,
   Button,
   useTheme,
-} from '@mui/material'
-import AddIcon from '@mui/icons-material/Add'
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import AddSources from "./AddSources";
+import { useState } from "react";
 
 const SourcesEmpty = () => {
-  const theme = useTheme()
+  const [addSourcesOpen, setAddSourcesOpen] = useState(false);
+  const theme = useTheme();
 
   return (
-    <Card sx={{ boxShadow: 'none', borderRadius: 0, minWidth: 262 }}>
-      <CardContent sx={{ padding: '24px', height: '100%', display: 'flex' }}>
-        <Stack direction="column">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h3" alignContent="center" textAlign="left">
-              Sources
-            </Typography>
-            <Button
-              variant="contained"
-              startIcon={<AddIcon fontSize="small" />}
-              sx={{
-                backgroundColor:
-                  theme.palette.mode === 'dark' ? '#EBEBEB' : '#1F2122',
-                borderRadius: '2px',
-                textTransform: 'none',
-                display: 'flex',
-                alignItems: 'center',
-                width: 'auto',
-                height: '32px',
-                minWidth: 0,
-              }}
+    <>
+      <Card sx={{ boxShadow: "none", borderRadius: 0, minWidth: 262 }}>
+        <CardContent sx={{ padding: "24px", height: "100%", display: "flex" }}>
+          <Stack direction="column">
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="h3" alignContent="center" textAlign="left">
+                Sources
+              </Typography>
+              <Button
+                variant="contained"
+                startIcon={<AddIcon fontSize="small" />}
+                onClick={() => setAddSourcesOpen(true)}
+                sx={{
+                  backgroundColor:
+                    theme.palette.mode === "dark" ? "#EBEBEB" : "#1F2122",
+                  borderRadius: "2px",
+                  textTransform: "none",
+                  display: "flex",
+                  alignItems: "center",
+                  width: "auto",
+                  height: "32px",
+                  minWidth: 0,
+                }}
+              >
+                Add
+              </Button>
+            </Stack>
+            <Typography
+              variant="body1"
+              textAlign="center"
+              width="100%"
+              marginTop="24px"
             >
-              Add
-            </Button>
+              Please connect a source using the 'Add' button
+            </Typography>
           </Stack>
-          <Typography
-            variant="body1"
-            textAlign="center"
-            width="100%"
-            marginTop="24px"
-          >
-            Please connect a source using the 'Add' button
-          </Typography>
-        </Stack>
-      </CardContent>
-    </Card>
-  )
-}
+        </CardContent>
+      </Card>
+      <AddSources
+        open={addSourcesOpen}
+        handleClose={() => setAddSourcesOpen(false)}
+      />
+    </>
+  );
+};
 
-export default SourcesEmpty
+export default SourcesEmpty;
