62c7279c8c0734f4bed4ac71b2f4814e91c5ba28
Tanner Scadden <tanner@scaddenfamily.com>
2024-08-05 19:01:48 -0400

    Formatted

--- packages/server/src/v1/pulse/infrastructure/database/knex.ts
+++ packages/server/src/v1/pulse/infrastructure/database/knex.ts
@@ -1,77 +1,77 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Knex, { Knex as KnexTypes } from "knex";
+import Knex, { Knex as KnexTypes } from 'knex'
 import {
   forOwn,
   isPlainObject,
   snakeCase,
   camelCase,
-} from "../../../../../utils/helpers.util";
-import { Module } from "@nestjs/common";
-import { KnexModule } from "nestjs-knex";
-import * as path from "path";
-import { BIN_PATH, DB_PATH, initDBDir } from "../../../../../utils/node.util";
+} from '../../../../../utils/helpers.util'
+import { Module } from '@nestjs/common'
+import { KnexModule } from 'nestjs-knex'
+import * as path from 'path'
+import { BIN_PATH, DB_PATH, initDBDir } from '../../../../../utils/node.util'
 
 const deepMapKeys = function (obj: any, fn: any) {
-  const x: { [key: string]: any } = {};
+  const x: { [key: string]: any } = {}
 
   forOwn(obj, function (v, k) {
     if (Array.isArray(v)) {
       v = v.map(function (x) {
-        return isPlainObject(x) ? deepMapKeys(x, fn) : x;
-      });
+        return isPlainObject(x) ? deepMapKeys(x, fn) : x
+      })
     }
     if (isPlainObject(v)) {
-      v = deepMapKeys(v, fn);
+      v = deepMapKeys(v, fn)
     }
-    x[fn(v, k)] = v;
-  });
+    x[fn(v, k)] = v
+  })
 
-  return x;
-};
+  return x
+}
 
 const postProcessResponse = (result: any) => {
   if (!result) {
-    return result;
+    return result
   }
-  return camelCaseKeys(result);
-};
+  return camelCaseKeys(result)
+}
 
 const wrapIdentifier = (value: any, origImpl: any) => {
   if (snakeCase(value)) {
-    return origImpl(snakeCase(value));
+    return origImpl(snakeCase(value))
   } else {
-    return origImpl(value);
+    return origImpl(value)
   }
-};
+}
 
 const camelCaseKeys = (obj: any) => {
   if (Array.isArray(obj)) {
     return obj.map((row) => {
-      return deepMapKeys(row, (v: any, k: any) => camelCase(k));
-    });
+      return deepMapKeys(row, (v: any, k: any) => camelCase(k))
+    })
   } else {
-    return deepMapKeys(obj, (v: any, k: any) => camelCase(k));
+    return deepMapKeys(obj, (v: any, k: any) => camelCase(k))
   }
-};
+}
 
-const IS_TEST = process.env.NODE_ENV === "test";
+const IS_TEST = process.env.NODE_ENV === 'test'
 
-initDBDir();
+initDBDir()
 
 // https://github.com/knex/knex/issues/1871#issuecomment-452342526
-export const SQL_LITE_TEST_FILE = "codeclimber.test.sqlite";
+export const SQL_LITE_TEST_FILE = 'codeclimber.test.sqlite'
 
 const knexConfig: KnexTypes.Config = {
-  client: "sqlite3",
+  client: 'sqlite3',
   connection: {
     filename: IS_TEST ? SQL_LITE_TEST_FILE : DB_PATH,
   },
   migrations: {
-    directory: path.join(BIN_PATH, "migrations"),
-    tableName: "knex_migrations",
+    directory: path.join(BIN_PATH, 'migrations'),
+    tableName: 'knex_migrations',
   },
   seeds: {
-    directory: path.join(BIN_PATH, "seeds"),
+    directory: path.join(BIN_PATH, 'seeds'),
   },
   useNullAsDefault: true,
   postProcessResponse, // Stuff coming back from the DB
@@ -91,13 +91,13 @@ const knexConfig: KnexTypes.Config = {
   //     console.log(message)
   //   },
   // },
-};
+}
 
-export const knex = Knex(knexConfig);
+export const knex = Knex(knexConfig)
 
 const knexModule = KnexModule.forRoot({
   config: knexConfig,
-});
+})
 
 @Module({
   imports: [knexModule],
