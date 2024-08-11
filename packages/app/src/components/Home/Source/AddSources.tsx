d6537f06d67e182f59ed18c0de02bb6ccad323e7
Tanner Scadden <44242867+Tanner-Scadden@users.noreply.github.com>
2024-08-11 13:13:39 -0600

    CLI: Add Sources Changes (#151)

--- packages/app/src/components/Home/Source/AddSources.tsx
+++ packages/app/src/components/Home/Source/AddSources.tsx
@@ -1,12 +1,17 @@
 import {
-  Modal,
   Typography,
   Box,
+  Button,
   IconButton,
-  Stack,
   Chip,
-  useTheme,
-  CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Skeleton,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  AccordionActions,
 } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import { useGetSources } from '../../../api/pulse.api'
@@ -15,67 +20,84 @@ import {
   supportedSources,
 } from '../../../utils/supportedSources'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { useState } from 'react'
+import { Refresh } from '@mui/icons-material'
+import { getTimeSince } from '../../../utils/time'
 
 interface AddSourcesRowProps {
   source: SourceDetails
-  connected: boolean
+  lastActive?: string
 }
 
-const AddSourceRow = ({ source, connected }: AddSourcesRowProps) => {
-  const [isExpanded, setIsExpanded] = useState(false)
-  const theme = useTheme()
-
-  const handleToggleExpanded = () => {
-    setIsExpanded(!isExpanded)
-  }
+const AddSourceRow = ({ source, lastActive }: AddSourcesRowProps) => {
+  const { refetch } = useGetSources()
 
   return (
-    <Box mb="16px">
-      <Stack
-        direction="row"
-        justifyContent="space-between"
+    <Accordion>
+      <AccordionSummary
+        aria-controls={`${source.name}-controls`}
+        id={`${source.name}-header`}
+        expandIcon={<ExpandMoreIcon />}
         sx={{
-          border: '1px solid #AAADB1',
-          padding: '16px',
-          borderRadius: '8px',
-          cursor: 'pointer',
+          '& .MuiAccordionSummary-content': {
+            width: '100%',
+            justifyContent: 'space-between',
+          },
         }}
-        onClick={handleToggleExpanded}
       >
-        <Stack direction="row" alignItems="center" spacing={1}>
-          <ExpandMoreIcon
-            sx={{
-              transition: 'transform 0.3s',
-              transform: isExpanded ? 'rotate(180deg)' : 'rotate(0deg)',
-              color: theme.palette.text.primary,
-            }}
-          />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
           <img
             alt={source.displayName + ' Logo'}
             src={source.logo}
             style={{ height: '32px', width: '32px' }}
           />
-          <Typography variant="body2" fontWeight={700}>
+          <Typography
+            variant="body2"
+            fontWeight={700}
+            component="a"
+            href={source.link}
+            target="_blank"
+            color="inherit"
+            onClick={(e) => e.stopPropagation()}
+          >
             {source.displayName}
           </Typography>
-        </Stack>
-        {connected && (
+        </Box>
+        {lastActive && (
           <Chip
-            label="Connected"
+            label={`Last Active: ${getTimeSince(lastActive)} ago`}
             color="primary"
             sx={{
-              color: '#FFFFFF',
+              mr: 2,
             }}
           />
         )}
-      </Stack>
-      {isExpanded && (
-        <Box sx={{ paddingX: '16px', paddingY: '0px' }}>
-          <div dangerouslySetInnerHTML={{ __html: source.instructions }} />
-        </Box>
-      )}
-    </Box>
+      </AccordionSummary>
+
+      <AccordionDetails>
+        <Box
+          component="ul"
+          sx={{
+            paddingRight: 2,
+            paddingLeft: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 0.5,
+            my: 0,
+            pt: 0,
+          }}
+          dangerouslySetInnerHTML={{ __html: source.instructions }}
+        />
+      </AccordionDetails>
+      <AccordionActions>
+        <Button
+          onClick={() => refetch()}
+          startIcon={<Refresh />}
+          color="primary"
+        >
+          Refresh
+        </Button>
+      </AccordionActions>
+    </Accordion>
   )
 }
 
@@ -86,93 +108,61 @@ interface AddSourcesProps {
 
 const AddSources = ({ open, handleClose }: AddSourcesProps) => {
   const { data: connectedSources, isPending, isError } = useGetSources()
-  const theme = useTheme()
 
   return (
-    <Modal
+    <Dialog
       open={open}
       onClose={handleClose}
-      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      maxWidth="sm"
+      fullWidth
+      aria-labelledby="sources-title"
     >
-      <Box
+      <DialogTitle sx={{ m: 0, p: 2 }} id="sources-title">
+        Add Sources
+      </DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
         sx={{
-          width: '80%',
-          maxWidth: 800,
-          height: '80%',
-          maxHeight: '80%',
-          bgcolor: theme.palette.background.paper,
-          p: 4,
-          borderRadius: '16px',
-          boxShadow: 24,
-          position: 'relative',
-          display: 'flex',
-          flexDirection: 'column',
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
         }}
       >
-        <IconButton
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            top: 16,
-            right: 16,
-            border: `1px solid ${theme.palette.text.primary}`,
-          }}
-        >
-          <CloseIcon sx={{ color: theme.palette.text.primary }} />
-        </IconButton>
-        <Typography
-          variant="h2"
-          fontWeight={700}
-          align="center"
-          marginBottom="32px"
-        >
-          Sources
-        </Typography>
+        <CloseIcon />
+      </IconButton>
+      <DialogContent>
         {isPending && (
-          <CircularProgress
-            sx={{
-              color: theme.palette.primary.main,
-              marginTop: '32px',
-              marginX: 'auto',
-            }}
-          />
-        )}
-        {isError && (
-          <Typography
-            color="#FFA726"
-            width="100%"
-            textAlign="center"
-            marginBottom="32px"
-          >
-            Error getting connected sources. All sources will be missing the
-            'Connected' chip.
-          </Typography>
+          <>
+            <Skeleton width="100%" height="80px" />
+            <Skeleton width="100%" height="80px" />
+          </>
         )}
         {!isPending && (
-          <Box
-            sx={{
-              flexGrow: 1,
-              overflowY: 'auto',
-              paddingRight: '8px',
-            }}
-          >
+          <>
             {supportedSources.map((source, index) => {
-              const connected =
-                connectedSources?.some(
-                  (connectedSource) => connectedSource.name === source.name,
-                ) || false
+              const lastActive = connectedSources?.find(
+                (connectedSource) => connectedSource.name === source.name,
+              )?.lastActive
               return (
                 <AddSourceRow
                   key={index}
                   source={source}
-                  connected={connected}
+                  lastActive={lastActive}
                 />
               )
             })}
-          </Box>
+          </>
         )}
-      </Box>
-    </Modal>
+        {isError && (
+          <Typography color="warning.main" textAlign="center" pt={2}>
+            Error getting connected sources. Sources will not show if they are
+            connected or not.
+          </Typography>
+        )}
+      </DialogContent>
+    </Dialog>
   )
 }
 
--- packages/app/src/utils/supportedSources.ts
+++ packages/app/src/utils/supportedSources.ts
@@ -1,10 +1,11 @@
 import vscodeLogo from '../public/source-logos/vscode.png'
 import chromeLogo from '../public/source-logos/chrome.webp'
-import firefoxLogo from '../public/source-logos/firefox.webp'
+// import firefoxLogo from "../public/source-logos/firefox.webp";
 
 export interface SourceDetails {
   name: string
   displayName: string
+  link: string
   logo: string
   instructions: string
 }
@@ -14,39 +15,39 @@ export const supportedSources: SourceDetails[] = [
     name: 'vscode',
     displayName: 'Visual Studio Code',
     logo: vscodeLogo,
+    link: 'https://marketplace.visualstudio.com/items?itemName=CodeClimbers.vscode-codeclimbers',
     instructions: `
-      <h2>Installing</h2>
-      <p>Press F1 or CMD + Shift + P and type install. Pick Extensions: Install Extension.</p>
-      <h2>Searching for plugin</h2>
-      <p>Type codeclimbers and hit enter.</p>
-      <p>Click Install.</p>
-      <p>You may need to reload the editor to activate the extension.</p>
+      <li>Press F1 or CMD + Shift + P and type install. Pick Extensions: Install Extension.</li>
+      <li>Type codeclimbers and hit enter.</li>
+      <li>Click Install.</li>
+      <li>You may need to reload the editor to activate the extension.</li>
     `,
   },
   {
     name: 'chrome-code_climbers',
-    displayName: 'Chrome',
+    displayName: 'Chromium Browser',
     logo: chromeLogo,
+    link: 'https://chromewebstore.google.com/search/codeclimbers',
     instructions: `
-      <h2>Installing</h2>
-      <p>Click the three dots in the top right corner of your browser.</p>
-      <p>Click More Tools, then Extensions.</p>
-      <p>Click the three dots in the top right corner of the Extensions page.</p>
-      <p>Click Open Chrome Web Store.</p>
-      <p>Search for CodeClimbers and click Add to Chrome.</p>
-      <p>Give the extension permissions to read and change site data if prompted.</p>
+      <li>Click the three dots in the top right corner of your browser.</li>
+      <li>Click More Tools, then Extensions.</li>
+      <li>Click the three dots in the top right corner of the Extensions page.</li>
+      <li>Click Open Chrome Web Store.</li>
+      <li>Search for CodeClimbers and click Add to Chrome.</li>
+      <li>Give the extension permissions to read and change site data if prompted.</li>
     `,
   },
-  {
-    name: 'firefox-code_climbers',
-    displayName: 'Firefox',
-    logo: firefoxLogo,
-    instructions: `
-      <h2>Installing</h2>
-      <p>Click the three lines in the top right corner of your browser.</p>
-      <p>Click Add-ons.</p>
-      <p>Search for CodeClimbers and click Add to Firefox.</p>
-      <p>Give the extension permissions to read and change site data if prompted.</p>
-    `,
-  },
+  // Firefox is not supported yet
+  // {
+  //   name: 'firefox-code_climbers',
+  //   displayName: 'Firefox',
+  //   logo: firefoxLogo,
+  //   instructions: `
+  //     <h2>Installing</h2>
+  //     <p>Click the three lines in the top right corner of your browser.</p>
+  //     <p>Click Add-ons.</p>
+  //     <p>Search for CodeClimbers and click Add to Firefox.</p>
+  //     <p>Give the extension permissions to read and change site data if prompted.</p>
+  //   `,
+  // },
 ]
