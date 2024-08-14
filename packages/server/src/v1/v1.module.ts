262792770fc20572bbfe06a33426f4d38b1b3fa4
Tanner Scadden <tanner@scaddenfamily.com>
2024-08-13 23:03:30 -0400

    Fixed lint issue

--- packages/server/src/v1/v1.module.ts
+++ packages/server/src/v1/v1.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common'
-import { HealthController } from '../common/infrastructure/http/controllers/health.controller'
-import { StartupController } from './startup/startup.controller'
-import { ActivitiesService } from './activities/activities.service'
-import { PulseController } from './activities/pulse.controller'
-import { WakatimeController } from './activities/wakatimeProxy.controller'
-import { PulseRepo } from './database/pulse.repo'
-import { DarwinStartupService } from './startup/darwinStartup.service'
-import { WindowStartupService } from './startup/windowStartup.service'}
-import { StartupServiceFactory } from './startup/startupService.factory'
-import { UnsupportedStartupService } from './startup/unsupportedStartup.service'
+import { Module } from "@nestjs/common";
+import { HealthController } from "../common/infrastructure/http/controllers/health.controller";
+import { StartupController } from "./startup/startup.controller";
+import { ActivitiesService } from "./activities/activities.service";
+import { PulseController } from "./activities/pulse.controller";
+import { WakatimeController } from "./activities/wakatimeProxy.controller";
+import { PulseRepo } from "./database/pulse.repo";
+import { DarwinStartupService } from "./startup/darwinStartup.service";
+import { WindowStartupService } from "./startup/windowStartup.service";
+import { StartupServiceFactory } from "./startup/startupService.factory";
+import { UnsupportedStartupService } from "./startup/unsupportedStartup.service";
 
 @Module({
   imports: [],
