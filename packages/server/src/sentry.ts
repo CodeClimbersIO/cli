684a03678207cb9d67619076d4ca4c455b774ef1
Paul Hovley <rphovley@gmail.com>
2024-08-13 19:18:54 -0600

    update DSN (#173)

--- packages/server/src/sentry.ts
+++ packages/server/src/sentry.ts
@@ -3,7 +3,7 @@ import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
-    dsn: 'https://00224a056c118536fd6c78e3df9619ed@o4507760376348672.ingest.us.sentry.io/4507760378118144',
+    dsn: 'https://e885e4c5eeed09d6229c7d7bfdc8d762@o4507772937043968.ingest.us.sentry.io/4507772946022400',
     integrations: [nodeProfilingIntegration()],
     // Performance Monitoring
     tracesSampleRate: 0.1, //  Capture 100% of the transactions
