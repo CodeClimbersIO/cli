1a2579e6507a4ce669766b472d435a7b37b46458
Paul Hovley <rphovley@gmail.com>
2024-09-13 09:24:53 -0600

    Always create a profile for users

--- packages/app/src/utils/posthog.util.ts
+++ packages/app/src/utils/posthog.util.ts
@@ -4,6 +4,6 @@ import posthog from 'posthog-js'
 export const initPosthog = () => {
   posthog.init('phc_jTQv0YoCvMEEtKYxlglm9Mo8cZgkfpvhIhRvvhTEXHY', {
     api_host: 'https://us.i.posthog.com',
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'always',
   })
 }
