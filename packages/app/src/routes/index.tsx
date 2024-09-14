a898d38c6b3c9917a2fc8221cfa0e2dce204e2db
Paul Hovley <rphovley@gmail.com>
2024-09-14 12:47:45 -0600

    better handling of install page response

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -4,9 +4,7 @@ import { useGetHealth } from '../api/health.api'
 import InstallPage from '../components/Home/InstallPage'
 
 function AppRouter() {
-  const { isLoading, isError, data: health } = useGetHealth()
-  if (isLoading) return <div>Loading...</div>
-  if (isError) return <div>Error</div>
+  const { data: health } = useGetHealth()
 
   let defaultRoute = <AppRoutes />
   if (!health) defaultRoute = <InstallPage />
