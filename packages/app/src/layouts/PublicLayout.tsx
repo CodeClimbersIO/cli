19124d254d43e77c4d8734902645b443bdaa717f
Paul Hovley <rphovley@gmail.com>
2024-09-15 21:27:50 -0600

    Add install route and seo (#216)

--- packages/app/src/layouts/PublicLayout.tsx
+++ packages/app/src/layouts/PublicLayout.tsx
@@ -1,12 +1,18 @@
 import { Box } from '@mui/material'
 import { Outlet } from 'react-router-dom'
+import { UpdateBanner } from '../components/common/UpdateBanner/UpdateBanner'
 
 interface BaseLayoutProps {
   children?: React.ReactNode
 }
 
 function BaseLayout({ children }: BaseLayoutProps) {
-  return <Box sx={{ padding: '2rem' }}>{children || <Outlet />}</Box>
+  return (
+    <Box sx={{ padding: '2rem' }}>
+      <UpdateBanner />
+      {children || <Outlet />}
+    </Box>
+  )
 }
 
 export default BaseLayout
--- packages/app/src/routes/AppRoutes.tsx
+++ packages/app/src/routes/AppRoutes.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import PublicLayout from '../layouts/PublicLayout'
 import { HomePage } from '../components/Home/HomePage'
 import { useAnalyticsPageSubscription } from '../hooks/useAnalytics'
-import { UpdateBanner } from '../components/common/UpdateBanner/UpdateBanner'
+import InstallPage from '../components/Home/InstallPage'
 
 // We want to subscribe to events in the router, but we don't want to rerender
 // the entire app when the route changes.
@@ -17,10 +17,12 @@ export const AppRoutes = () => {
   return (
     <>
       <AppRoutesPageSubscription />
-      <UpdateBanner />
       <Routes>
         <Route element={<PublicLayout />}>
           <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route>
+          <Route path="/install" element={<InstallPage />} />
         </Route>
       </Routes>
     </>
