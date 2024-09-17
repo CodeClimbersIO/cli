de4a21e1e5992505e35eae615880b38f631b42e2
Nenad Vajagic <50087069+nvajagic@users.noreply.github.com>
2024-09-17 23:05:46 +0200

    feat(app): Add double click for showing projects and add hover effect on the bar

--- packages/app/src/components/Home/Time/TimeDataChart.tsx
+++ packages/app/src/components/Home/Time/TimeDataChart.tsx
@@ -2,14 +2,11 @@ import {
   Box,
   Collapse,
   emphasize,
-  IconButton,
   LinearProgress,
   rgbToHex,
   Typography,
 } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import { useCallback, useState } from 'react'
 
 interface SubCategory {
@@ -26,12 +23,61 @@ interface TimeDataChartProps {
   subCategories?: SubCategory[]
 }
 
+const Progress = ({
+  progress,
+  hasSubCategories,
+  color,
+}: {
+  progress: number
+  hasSubCategories?: boolean
+  color: string
+}) => {
+  const progressBars = []
+  let remainingProgress = progress
+  const laps = Math.ceil(progress / 100)
+  let currentColor = color
+  for (let i = 0; i < laps; i++) {
+    const height = (laps - i) / laps
+    const assignedColor = rgbToHex(currentColor)
+    progressBars.push(
+      <LinearProgress
+        variant="determinate"
+        value={remainingProgress > 100 ? 100 : remainingProgress}
+        key={i}
+        sx={{
+          position: 'relative',
+          width: '100%',
+          margin: '0 auto',
+          py: 1.5,
+          borderRadius: 1,
+          backgroundColor: 'transparent',
+          '& .MuiLinearProgress-bar': {
+            bottom: 0,
+            top: 'auto',
+            backgroundColor: `${assignedColor}`,
+            borderRadius: 1,
+            height: height * 100 + '%',
+            ...(hasSubCategories && {
+              '&:hover': {
+                backgroundColor: emphasize(currentColor, 0.4),
+              },
+            }),
+          },
+        }}
+      />,
+    )
+    currentColor = emphasize(currentColor, 0.3)
+    remainingProgress -= 100
+  }
+  return <Box sx={{ position: 'relative' }}>{progressBars}</Box>
+}
+
 export const TimeDataChart = ({
   title,
   color,
   progress,
   time,
-  subCategories
+  subCategories,
 }: TimeDataChartProps) => {
   const [expanded, setExpanded] = useState(false)
 
@@ -39,91 +85,75 @@ export const TimeDataChart = ({
     setExpanded((prev) => !prev)
   }, [])
 
-  const Progress = ({ progress }: { progress: number }) => {
-    const progressBars = []
-    let remainingProgress = progress
-    const laps = Math.ceil(progress / 100)
-    let currentColor = color
-    for (let i = 0; i < laps; i++) {
-      const height = (laps - i) / laps
-      const assignedColor = rgbToHex(currentColor)
-      progressBars.push(
-        <LinearProgress
-          variant="determinate"
-          value={remainingProgress > 100 ? 100 : remainingProgress}
-          key={i}
-          sx={{
-            position: 'absolute',
-            width: '100%',
-            py: 1.5,
-            borderRadius: 1,
-            backgroundColor: 'transparent',
-            '& .MuiLinearProgress-bar': {
-              bottom: 0,
-              top: 'auto',
-              backgroundColor: `${assignedColor}`,
-              borderRadius: 1,
-              height: height * 100 + '%',
-            },
-          }}
-        />,
-      )
-      currentColor = emphasize(currentColor, 0.3)
-      remainingProgress -= 100
-    }
-    return <Box sx={{ position: 'relative' }}>{progressBars}</Box>
-  }
+  const hasSubCategories = subCategories && subCategories.length > 0
+
   return (
-    <Grid2>
+    <Box>
       <Grid2
         container
         justifyContent="space-between"
         gap={3}
         alignItems="center"
+        {...(hasSubCategories && {
+          onClick: handleExpand,
+          sx: {
+            cursor: 'pointer',
+          },
+        })}
       >
         <Grid2 sx={{ minWidth: 120 }}>
           <Typography variant="body1">{title}</Typography>
         </Grid2>
         <Grid2 sx={{ flex: 1 }}>
-          <Progress progress={progress} />
+          <Progress
+            progress={progress}
+            color={color}
+            hasSubCategories={hasSubCategories}
+          />
         </Grid2>
         <Grid2>
           <Typography variant="body1" fontWeight="bold">
             {time}
           </Typography>
         </Grid2>
-        {subCategories && (
-          <IconButton onClick={handleExpand}>
-            {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </IconButton>
-        )}
       </Grid2>
-      <Collapse in={expanded}>
-        <Box mt={2} ml={4} sx={{ fontSize: '0.9em' }}>
-          <Grid2 container direction="column" spacing={1}>
-            {subCategories?.map((subCategory, index) => (
-              <Grid2 key={index}>
-                <Grid2
-                  container
-                  justifyContent="space-between"
-                  gap={1}
-                  alignItems="center"
-                >
-                  <Grid2 sx={{ minWidth: 120 }}>
-                    <Typography variant="body2">{subCategory.title}</Typography>
+      {hasSubCategories && (
+        <Collapse in={expanded}>
+          <Box mt={2} ml={4} sx={{ fontSize: '0.9em' }}>
+            <Grid2 container direction="column" spacing={1}>
+              {subCategories?.map((subCategory, index) => {
+                return (
+                  <Grid2 key={index}>
+                    <Grid2
+                      container
+                      justifyContent="space-between"
+                      gap={1}
+                      alignItems="center"
+                    >
+                      <Grid2 sx={{ minWidth: 120 }}>
+                        <Typography variant="body2">
+                          {subCategory.title}
+                        </Typography>
+                      </Grid2>
+                      <Grid2 sx={{ flex: 1 }}>
+                        <Progress
+                          progress={subCategory.progress}
+                          color={color}
+                        />
+                      </Grid2>
+                      <Grid2>
+                        <Typography variant="body2">
+                          {subCategory.time}
+                        </Typography>
+                      </Grid2>
+                    </Grid2>
                   </Grid2>
-                  <Grid2 sx={{ flex: 1 }}>
-                    <Progress progress={subCategory.progress} />
-                  </Grid2>
-                  <Grid2>
-                    <Typography variant="body2">{subCategory.time}</Typography>
-                  </Grid2>
-                </Grid2>
-              </Grid2>
-            ))}
-          </Grid2>
-        </Box>
-      </Collapse>
-    </Grid2>
+                )
+              })}
+            </Grid2>
+          </Box>
+        </Collapse>
+      )}
+    </Box>
   )
 }
