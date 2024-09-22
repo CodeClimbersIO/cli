5251a8527087caa59139ee72de26e08fedf296ea
Paul Hovley <rphovley@gmail.com>
2024-09-22 15:53:22 -0600

    only return apikey when it has a value

--- packages/app/src/utils/auth.util.ts
+++ packages/app/src/utils/auth.util.ts
@@ -1,10 +1,15 @@
 // function to get api key from local storage
 const LOCAL_API_KEY = 'local_api_key'
 function getLocalApiKey() {
-  return localStorage.getItem(LOCAL_API_KEY)
+  const apiKey = localStorage.getItem(LOCAL_API_KEY)
+  if (apiKey === 'undefined') {
+    return null
+  }
+  return apiKey
 }
 
 function setLocalApiKey(apiKey: string) {
+  if (apiKey === 'undefined') return
   localStorage.setItem(LOCAL_API_KEY, apiKey)
 }
 
