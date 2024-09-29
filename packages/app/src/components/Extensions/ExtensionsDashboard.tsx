16aff4ab034d7bb9a4582ceac88a69f04a8e17f5
Paul Hovley <rphovley@gmail.com>
2024-09-29 12:02:56 -0600

    Add card for "adding extension"

--- packages/app/src/components/Extensions/ExtensionsDashboard.tsx
+++ packages/app/src/components/Extensions/ExtensionsDashboard.tsx
@@ -3,16 +3,24 @@ import extensionsService, {
   DashboardExtension,
 } from '../../services/extensions.service'
 import Grid2 from '@mui/material/Unstable_Grid2'
+import CodeClimbersButton from '../common/CodeClimbersButton'
+import { useNavigate } from 'react-router-dom'
 
 export const ExtensionsDashboard = () => {
   const extensions = extensionsService.getActiveDashboardExtensions()
+  const navigate = useNavigate()
 
   const ExtensionCard = ({ extension }: { extension: DashboardExtension }) => {
     return (
       <Grid2 sm={12} lg={6}>
         <Card
           raised={false}
-          sx={{ boxShadow: 'none', borderRadius: 0, width: '100%' }}
+          sx={{
+            boxShadow: 'none',
+            borderRadius: 0,
+            width: '100%',
+            height: '300px',
+          }}
         >
           <CardContent
             sx={{
@@ -31,10 +39,39 @@ export const ExtensionsDashboard = () => {
 
   return (
     <Box sx={{ mt: 2 }}>
-      <Grid2 container>
+      <Grid2 container spacing={4}>
         {extensions.map((extension) => (
           <ExtensionCard key={extension.id} extension={extension} />
         ))}
+        {/* if extensions will only have one component, add an additional component to the grid to "add an extension" */}
+        {extensions.length % 2 === 1 && (
+          <Grid2 sm={12} lg={6}>
+            <Card
+              raised={false}
+              sx={{
+                boxShadow: 'none',
+                borderRadius: 0,
+                width: '100%',
+                height: '300px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <CardContent>
+                <CodeClimbersButton
+                  eventName="dashboard-add-extension"
+                  variant="text"
+                  onClick={() => {
+                    navigate('/extensions')
+                  }}
+                >
+                  Add extension
+                </CodeClimbersButton>
+              </CardContent>
+            </Card>
+          </Grid2>
+        )}
       </Grid2>
     </Box>
   )
