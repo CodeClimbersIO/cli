51504d60f1ff7ad2a42df0348507efb456f5e860
Paul Hovley <rphovley@gmail.com>
2024-10-03 18:35:57 -0600

    fix user migrations

--- bin/migrations/20241003221416_add_user.js
+++ bin/migrations/20241003221416_add_user.js
@@ -8,9 +8,23 @@ const SQL = `--sql
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
   );
+
 `
-exports.up = function (knex) {
-  return knex.raw(SQL)
+
+const SQL_SETTINGS = `--sql
+  CREATE TABLE IF NOT EXISTS accounts_user_settings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    weekly_report_type VARCHAR(255) NOT NULL DEFAULT 'none',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES accounts_user (id) ON DELETE CASCADE
+  );
+  `
+exports.up = async function (knex) {
+  await knex.raw(SQL)
+  await knex.raw(SQL_SETTINGS)
+  return
 }
 
 exports.down = function (knex) {
--- bin/migrations/20241004000814_create_user.js
+++ bin/migrations/20241004000814_create_user.js
@@ -1,11 +1,26 @@
-const SQL = `--sql
-  INSERT INTO accounts_user DEFAULT VALUES;
-  
-  INSERT INTO accounts_user_settings (user_id)
-  VALUES ((SELECT last_insert_rowid()));
-`
-exports.up = function (knex) {
-  return knex.raw(SQL)
-}
+exports.up = function(knex) {
+  return knex.transaction(async (trx) => {
+    // Insert into accounts_user
+    await trx('accounts_user').insert({});
+    const [row] = await trx.raw('SELECT last_insert_rowid() as id');
+    const userId = row.id;
+    // Insert into accounts_user_settings
+    await trx('accounts_user_settings').insert({
+      user_id: userId
+    });
+  });
+};
 
-exports.down = function () {}
+exports.down = function(knex) {
+  // return knex.transaction(async (trx) => {
+  //   // Remove the last inserted user_settings
+  //   await trx('accounts_user_settings')
+  //     .where('user_id', knex.raw('(SELECT MAX(id) FROM accounts_user)'))
+  //     .del();
+
+  //   // Remove the last inserted user
+  //   await trx('accounts_user')
+  //     .where('id', knex.raw('(SELECT MAX(id) FROM accounts_user)'))
+  //     .del();
+  // });
+};
