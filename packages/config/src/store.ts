707ded0b47a00fa9cb8a56e7c09924d0748b673a
Paul Hovley <rphovley@gmail.com>
2024-10-21 20:47:55 -0600

    remove config and fix linting (#253)

--- packages/config/src/store.ts
+++
@@ -1,7 +0,0 @@
-export interface Store {
-  put(key: string, enabled: boolean): Promise<void>
-  get(key: string): boolean
-  has(key: string): boolean
-  id(): string
-  mayUpdateOnRestart(): Promise<boolean>
-}
