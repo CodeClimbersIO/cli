d27e1b3f0c61aaf8f6c089f67d651f98b8ef5377
Danielle Hovley <djl.hovley@gmail.com>
2024-10-24 15:57:13 -0600

    added data for multiple categories; refined chart; created utils file to handle data refining and the like; cleaned up index file;

--- packages/app/src/extensions/HourlyCategoryReport/hourlyCategoryReport.api.ts
+++ packages/app/src/extensions/HourlyCategoryReport/hourlyCategoryReport.api.ts
@@ -1,19 +1,46 @@
 import { sqlQueryFn } from '@app/api/browser/services/query.service'
 import { useMutation } from '@tanstack/react-query'
 
-export const useGetData = (startDate: string, endDate: string) => {
-  const query = `WITH getMinutes (minutes, time) AS (
-      SELECT category, time
-      FROM activities_pulse
-      WHERE activities_pulse.time BETWEEN '${startDate}' AND '${endDate}'
-        and (category is 'coding' or category is 'debugging')
-      GROUP BY strftime('%s', time) / 120)
-  SELECT time, count() * 2 as minutes
+const getQuery = (startDate: string, endDate: string, category: string) => 
+  `WITH getMinutes (category, time) AS (
+    SELECT category, time
+    FROM activities_pulse
+    WHERE activities_pulse.time BETWEEN '${startDate}' AND '${endDate}'
+      AND ${category}
+    GROUP BY strftime('%s', time) / 120)
+  SELECT category, time, count() * 2 AS minutes
   FROM getMinutes
   GROUP BY strftime('%s', time) / 3600
   ORDER BY time;`
 
+export const useGetCodingData = (startDate: string, endDate: string) => {
+  const query = getQuery(startDate, endDate, "(category is 'coding' or category is 'debugging')")
+
   return useMutation({
-    mutationFn: () => sqlQueryFn(query, 'hourlyCategoryReport'),
+    mutationFn: () => sqlQueryFn(query, 'hourlyCategoryReport-coding'),
+  })
+}
+
+export const useGetBrowsingData = (startDate: string, endDate: string) => {
+  const query = getQuery(startDate, endDate, "category is 'browsing'")
+
+  return useMutation({
+    mutationFn: () => sqlQueryFn(query, 'hourlyCategoryReport-browsing'),
+  })
+}
+
+export const useGetCommunicatingData = (startDate: string, endDate: string) => {
+  const query = getQuery(startDate, endDate, "category is 'communicating'")
+
+  return useMutation({
+    mutationFn: () => sqlQueryFn(query, 'hourlyCategoryReport-communicating'),
+  })
+}
+
+export const useGetDesigningData = (startDate: string, endDate: string) => {
+  const query = getQuery(startDate, endDate, "category is 'designing'")
+
+  return useMutation({
+    mutationFn: () => sqlQueryFn(query, 'hourlyCategoryReport-designing'),
   })
 }
---
+++ packages/app/src/extensions/HourlyCategoryReport/hourlyCategoryReport.utils.ts
@@ -0,0 +1,126 @@
+import { GraphColors } from '@app/config/theme';
+import {
+  categories,
+  simplifiedCategories,
+  typeColors,
+} from '@app/utils/categories'
+import { Theme } from '@mui/material'
+import dayjs from 'dayjs'
+
+export type HourlyResponse = { category: string; time: string; minutes: number }
+export type DataPoint = { x: string; y: number } // x: hour, y: minutes
+export type ChartData = {
+  id: string
+  color: string
+  data: DataPoint[]
+  rawData?: HourlyResponse[]
+}
+
+const getXLabel = (hour: number): string => {
+  if (hour < 12) return `${hour} am`
+  if (hour === 12) return `${hour} pm`
+  return `${hour - 12} pm`
+}
+
+export const formatData = (
+  hourlyData: HourlyResponse[],
+  theme: Theme,
+): ChartData[] => {
+  if (hourlyData?.length === 0) return []
+
+  const timeByCategory = Object.values(simplifiedCategories).map(
+    (category) => ({
+      id: category,
+      color:
+        typeColors(theme).find((typeColor) => typeColor.category === category)
+          ?.color ?? theme.palette.graphColors.blue,
+      rawData: hourlyData.filter((item) => {
+        if (category === categories.coding)
+          return (
+            item.category === category || item.category === categories.debugging
+          )
+        return item.category === category
+      }),
+      data: [] as DataPoint[],
+    }),
+  )
+
+  const firstHour = dayjs(hourlyData[0]?.time).hour()
+  const lastHour = dayjs(hourlyData[hourlyData.length - 1]?.time).hour()
+  const length = lastHour - firstHour + 1
+
+  timeByCategory.forEach((category) => {
+    if (category.rawData.length === 0) return
+
+    const basicHours = Array.from({ length }, (_, index) => ({
+      x: getXLabel(index + firstHour),
+      y:
+        category.rawData.find(
+          (hour) => dayjs(hour.time).hour() === index + firstHour,
+        )?.minutes ?? 0,
+    }))
+    category.data = basicHours
+  })
+
+  return timeByCategory
+}
+
+export const getYIntervals = (res: HourlyResponse[]): number[] => {
+  const max = Math.max(...res.map((hour) => hour.minutes))
+
+  if (max <= 5) {
+    const length = max + 1
+    return Array.from({ length }, (v, i) => i)
+  }
+  if (max < 20) {
+    const length = max / 2 + 1
+
+    if (max % 2 > 0) {
+      return Array.from({ length }, (v, i) => {
+        return Math.round(i * 2) + 1
+      })
+    }
+
+    return Array.from({ length }, (v, i) => {
+      return Math.round(i * 2)
+    })
+  }
+
+  const interval = max / 8
+  return Array.from({ length: 9 }, (v, i) => {
+    if (i === 8) {
+      return max
+    }
+    return Math.round(i * interval)
+  })
+}
+
+export const getChartTheme = (graphColors: GraphColors) => ({
+  text: { fill: graphColors.grey },
+  axis: {
+    ticks: {
+      text: {
+        fill: graphColors.grey,
+      },
+      line: {
+        stroke: graphColors.grey,
+      },
+    },
+    domain: {
+      line: {
+        stroke: graphColors.grey,
+      },
+    },
+    legend: {
+      text: {
+        fill: graphColors.grey,
+      },
+    },
+  },
+  tooltip: {
+    container: {
+      color: 'black',
+      fontSize: 12,
+    },
+  },
+})
